7d3275747138a3dadaf6a7eb8d5036b2422f7330
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-06-07 16:37:45 -0700

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -32,9 +32,9 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
-    if !params[:pet].keys.include?("owner_id")
-      pramas[:pet]["owner_id"] = []
-    end
+    # if !params[:pet].keys.include?("owner_id")
+    #   pramas[:pet]["owner_id"] = []
+    # end
 
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
